2d1e56b94c26b0278be1c6d5b57838e7614bf9c3
vendetta <okta.pilopa@staff.itera.ac.id>
2019-10-21 09:48:57 +0700

    new activity

--- app/src/main/java/id/my/cariberas/pengeluaranharian/MainActivity.java
+++ app/src/main/java/id/my/cariberas/pengeluaranharian/MainActivity.java
@@ -2,13 +2,31 @@ package id.my.cariberas.pengeluaranharian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    // inisialisasi fab
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //add
+        FloatingActionButton floatingActionButton=findViewById(R.id.fab1);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Floating Action Button Berhasil dibuat", Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(MainActivity.this,TambahPengeluaranActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
