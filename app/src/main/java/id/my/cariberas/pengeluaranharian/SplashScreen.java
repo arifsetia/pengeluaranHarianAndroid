28f69da6b372401af2ded8835e67c1be023a9c53
v3nd3tt4 <pilopaokta@gmail.com>
2019-10-23 00:00:51 +0700

    splash screen

---
+++ app/src/main/java/id/my/cariberas/pengeluaranharian/SplashScreen.java
@@ -0,0 +1,29 @@
+package id.my.cariberas.pengeluaranharian;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L); //3000 L = 3 detik
+    }
+}
