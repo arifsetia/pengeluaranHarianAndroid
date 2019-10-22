7ae9775fef61e1589fb6c8fe82b02b88f6686591
v3nd3tt4 <pilopaokta@gmail.com>
2019-10-22 23:26:07 +0700

    detail laporan

--- app/src/main/java/id/my/cariberas/pengeluaranharian/DetailLaporanLainnya.java
+++ app/src/main/java/id/my/cariberas/pengeluaranharian/DetailLaporanLainnya.java
@@ -1,13 +1,25 @@
 package id.my.cariberas.pengeluaranharian;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,34 +27,52 @@ import android.widget.Toast;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 
-public class DetailLaporanLainnya extends AppCompatActivity {
+public class DetailLaporanLainnya extends AppCompatActivity implements GroceryRecyclerViewAdapter.RecyclerImageAdapter{
     DatabaseHelper dbcenter;
-    public static DetailLaporanLainnya dll;
+    public static DetailLaporanLainnya ma;
     protected Cursor cursor;
+
+    private RecyclerView recyclerView;
+    private GroceryRecyclerViewAdapter adapter;
+    private ArrayList<Grocery> groceryArrayList;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    AlertDialog.Builder dialog;
+    LayoutInflater inflater;
+    View dialogView;
+
+    private String t1,t2;
+
+    DatePickerDialog picker;
 
     Locale localeID = new Locale("in", "ID");
     private NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_laporan_lainnya);
 
-        dll = this;
+        ma = this;
         dbcenter = new DatabaseHelper(this);
 
         Intent intent = getIntent();
 
 
-        String tanggalAwal = getIntent().getStringExtra("tanggalAwal");
-        String tanggalAkhir = getIntent().getStringExtra("tanggalAkhir");
+        final String tanggalAwal = getIntent().getStringExtra("tanggalAwal");
+        final String tanggalAkhir = getIntent().getStringExtra("tanggalAkhir");
+
+        this.t1 = tanggalAwal;
+        this.t2 = tanggalAkhir;
+
 //        Toast.makeText(DetailLaporanLainnya.this, tanggalAkhir, Toast.LENGTH_LONG).show();
         GetPengeluaranBulanIni(tanggalAwal, tanggalAkhir);
+        RefreshList(tanggalAwal, tanggalAkhir);
 
         ImageView imgHalamanUtama = findViewById(R.id.btnHalamanUtama);
         imgHalamanUtama.setOnClickListener(new View.OnClickListener() {
@@ -53,7 +83,15 @@ public class DetailLaporanLainnya extends AppCompatActivity {
             }
         });
 
-
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                RefreshList(tanggalAwal, tanggalAkhir);
+                GetPengeluaranBulanIni(tanggalAwal, tanggalAkhir);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 
     public void GetPengeluaranBulanIni(String firstDate, String secondDate){
@@ -94,5 +132,139 @@ public class DetailLaporanLainnya extends AppCompatActivity {
 
         return outputDate;
 
+    }
+
+    public void RefreshList(String firstDate, String secondDate) {
+        SQLiteDatabase db = dbcenter.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM tb_pengeluaran where category = 'pengeluaran'  and date BETWEEN '"+firstDate+"' AND '"+secondDate+"';", null);
+        cursor.moveToFirst();
+
+        ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+        for (int cc = 0; cc < cursor.getCount(); cc++) {
+            cursor.moveToPosition(cc);
+            try {
+                HashMap<String, String> showData = new HashMap<String, String>();
+                showData.put("id_pengeluaran", cursor.getString(0).toString());
+                showData.put("title", cursor.getString(1).toString());
+                showData.put("date", cursor.getString(2).toString());
+                showData.put("fee", "Rp. "+cursor.getString(3).toString());
+                list.add(showData);
+            } catch (Exception e) {
+                Log.d("Exception : ",e.getMessage());
+            }
+        }
+
+        recyclerView = (RecyclerView) findViewById(R.id.rvPengeluaranDetail);
+        adapter = new GroceryRecyclerViewAdapter(new ArrayList<HashMap>(list), this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onRecyclerImageSelected(HashMap groceryList) {
+        String xImg = groceryList.get("fee").toString();
+        final String idPengeluaran = groceryList.get("id_pengeluaran").toString();
+        AlertDialog.Builder alert = new AlertDialog.Builder(DetailLaporanLainnya.this);
+        alert.setTitle("Hapus Data");
+        alert.setMessage("Apakah anda yakin akan menghapus data ini?");
+        alert.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SQLiteDatabase db = dbcenter.getReadableDatabase();
+                db.delete("tb_pengeluaran","id_pengeluaran=? ",new String[]{idPengeluaran});
+                cursor.moveToFirst();
+                Toast.makeText(DetailLaporanLainnya.this, "Data Berhasil Dihapus", Toast.LENGTH_SHORT).show();
+                RefreshList(t1, t2);
+                GetPengeluaranBulanIni(t1, t2);
+                dialog.dismiss();
+            }
+        });
+
+        alert.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alert.show();
+    }
+
+    public void onRecyclerLongClick(HashMap groceryList){
+
+//        dialog = new AlertDialog.Builder(MainActivity.this);
+//        inflater = getLayoutInflater();
+//        dialogView = inflater.inflate(R.layout.activity_edit_data, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.activity_edit_data,null);
+
+        builder.setView(dialogView);
+        builder.setCancelable(true);
+
+        final AlertDialog dialog = builder.create();
+
+        final String id_pengeluaran = groceryList.get("id_pengeluaran").toString();
+        final String title = groceryList.get("title").toString();
+        final String date = groceryList.get("date").toString();
+        final String fee = groceryList.get("fee").toString();
+
+        String currentString = fee;
+        String[] separated = currentString.split(". ");
+        final String feeBaru = separated[1].trim();
+
+        //        EditText id_pengeluaran_edit = dialogView.findViewById(R.id.tPengeluaranEdit);
+        final EditText titleEdit = dialogView.findViewById(R.id.tPengeluaranEdit);
+        final EditText feeEdit = dialogView.findViewById(R.id.tHargaEdit);
+        final EditText tanggalEdit = dialogView.findViewById(R.id.tTanggalEdit);
+        Button btnUpdate = dialogView.findViewById(R.id.btnUpdate);
+
+
+        //google calendar
+        tanggalEdit.setInputType(InputType.TYPE_NULL);
+        tanggalEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(DetailLaporanLainnya.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+//                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                tanggalEdit.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        titleEdit.setText(title);
+        feeEdit.setText(feeBaru);
+        tanggalEdit.setText(date);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbcenter.getReadableDatabase();
+                ContentValues cv = new ContentValues();
+                cv.put("title", titleEdit.getText().toString());
+                cv.put("date", tanggalEdit.getText().toString());
+                cv.put("fee", feeEdit.getText().toString());
+                db.update("tb_pengeluaran", cv, "id_pengeluaran="+id_pengeluaran, null);
+                cursor.moveToFirst();
+                Toast.makeText(DetailLaporanLainnya.this, "Data Berhasil Diupdate", Toast.LENGTH_SHORT).show();
+                RefreshList(t1, t2);
+                GetPengeluaranBulanIni(t1, t2);
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
