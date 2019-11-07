ed9d369ff422f4d5224007c3095ec2a0669971c2
vendetta <okta.pilopa@staff.itera.ac.id>
2019-11-07 09:18:17 +0700

    from pc itera

--- app/src/main/java/id/my/cariberas/pengeluaranharian/SplashScreen.java
+++ app/src/main/java/id/my/cariberas/pengeluaranharian/SplashScreen.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
-    private int waktu_loading=2000;
+    private int waktu_loading=10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        TextView teks = findViewById(R.id.txtNmApk);
+        Typeface customfont=Typeface.createFromAsset(getAssets(),"font/NeoSansStd Bold TR.otf");
+        teks.setTypeface(customfont);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
